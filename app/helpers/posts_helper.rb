4f33c99aa0e6877461160ee9ad96f083c27f69c2
Boots2291 <buteramv@gmail.com>
2019-04-03 13:05:11 -0400

    Add specs for the update_pagination_partial_path helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -33,9 +33,9 @@ module PostsHelper
 
   def update_pagination_partial_path
     if @posts.next_page
-      'posts/post/posts_pagination_page/update_pagination'
+      'posts/posts_pagination_page/update_pagination'
     else
-      'posts/post/posts_pagination_page/remove_pagination'
+      'posts/posts_pagination_page/remove_pagination'
     end
   end
 end
